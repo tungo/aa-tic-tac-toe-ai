f15e43e31ae7521356a40ed4977d1f662be438c3
Tu Ngo <tungo8810@gmail.com>
2017-03-31 17:37:18 -0700

    Final commit

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -11,6 +11,14 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    # debugger
+    winner = board.winner
+    return false if board.over? && winner.nil?
+    return true if board.over? && winner != evaluator
+
+    children.all? do |child_node|
+      child_node.losing_node?(evaluator)
+    end
   end
 
   def winning_node?(evaluator)
@@ -24,7 +32,8 @@ class TicTacToeNode
     board.rows.each_with_index do |row, row_i|
       row.each_with_index do |square, col_i|
         if square.nil?
-          next_board = board.dup[[row_i, col_i]] = next_mover_mark
+          next_board = board.dup
+          next_board[[row_i, col_i]] = next_mover_mark
           kids << TicTacToeNode.new(next_board, children_mover_mark, [row_i, col_i])
         end
       end
